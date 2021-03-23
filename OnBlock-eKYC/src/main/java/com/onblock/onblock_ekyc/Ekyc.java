3278a5602e76e01b639b1a1f97b0853c1226be39
LoloziAnas <lolozianas1@gmail.com>
2021-03-23 12:23:34 +0100

    first commit

---
+++ OnBlock-eKYC/src/main/java/com/onblock/onblock_ekyc/Ekyc.java
@@ -0,0 +1,10 @@
+package com.onblock.onblock_ekyc;
+
+import android.util.Log;
+
+public class Ekyc {
+    public static String TAG = "Ekyc.jva";
+    public static void Log(){
+        Log.d(TAG, "message from eKYC library");
+    }
+}
